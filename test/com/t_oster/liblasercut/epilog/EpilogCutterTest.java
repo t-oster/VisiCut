2854e77eb96a155b3c01722775b0945465cd1df5
Thomas Oster <thommy.oster@arcor.de>
2011-05-16 20:20:39 +0200

    Adapted testcase to new Job Format

--- test/com/t_oster/liblasercut/epilog/EpilogCutterTest.java
+++ test/com/t_oster/liblasercut/epilog/EpilogCutterTest.java
@@ -26,10 +26,11 @@ public class EpilogCutterTest {
         
         EpilogCutter instance = new EpilogCutter("137.226.56.228");
         VectorPart vp = new VectorPart(5000,20,100);
-        vp.line(0,0,0,100);
-        vp.line(0,100,100,100);
-        vp.line(100,100,100,0);
-        vp.line(100,0,0,0);
+        vp.moveto(0, 0);
+        vp.lineto(100, 0);
+        vp.lineto(100,100);
+        vp.lineto(0,100);
+        vp.lineto(0,0);
         LaserJob job = new LaserJob("testpilog", "666", "bla", 500, vp);
         instance.sendJob(job);
     }
