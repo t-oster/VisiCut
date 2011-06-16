0e279d3667d228761648782de6bf5d0d09c528e0
Thomas Oster <oster@chambers.informatik.rwth-aachen.de>
2011-06-16 18:55:51 +0200

    refactoring and focus in raster

--- test/com/t_oster/liblasercut/epilog/EpilogCutterTest.java
+++ test/com/t_oster/liblasercut/epilog/EpilogCutterTest.java
@@ -4,8 +4,13 @@
  */
 package com.t_oster.liblasercut.epilog;
 
+import java.awt.Font;
+import com.t_oster.util.BufferedImageAdapter;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import com.t_oster.util.Point;
 import com.t_oster.liblasercut.*;
+import java.awt.Color;
 import java.util.LinkedList;
 import java.util.List;
 import org.junit.Test;
@@ -42,6 +47,31 @@ public class EpilogCutterTest
     assertEquals((byte) -128, (byte) 0x80);
   }
 
+  private BufferedImage getTestImage()
+    {
+      BufferedImage testbild = new BufferedImage(300,300, BufferedImage.TYPE_INT_RGB);
+    Graphics g = testbild.getGraphics();
+    g.setColor(Color.WHITE);
+    g.fillRect(0, 0, 300, 300);
+    g.setColor(Color.BLACK);
+    g.drawRect(0, 0, 299, 299);
+    g.setFont(new Font("sansserif", Font.BOLD, 12));
+    g.drawString("The quick brown fox jumps", 0, 20);
+    g.drawString("over the lazy dog.", 0, 40);
+    g.fillRect(0, 150, 75, 75);
+    g.setColor(Color.WHITE);
+    g.fillRect(75, 150, 75, 75);
+    g.setColor(Color.DARK_GRAY);
+    g.fillRect(150, 150, 75, 75);
+    g.setColor(Color.LIGHT_GRAY);
+    g.fillRect(3*75, 150, 75, 75);
+    for (int x=0;x<300;x++){
+        g.setColor(new Color(255*x/300,255*x/300,255*x/300));
+        g.drawLine(x, 200, x, 300);
+    }
+    return testbild;
+  }
+
   /**
    * This method sends a job which contains all three parts (raster,raster3d,vector)
    * and also tests focus change on every part
@@ -53,6 +83,7 @@ public class EpilogCutterTest
     System.out.println("sendJob");
 
     EpilogCutter instance = new EpilogCutter("137.226.56.228");
+    System.out.println("Creating VP");
     VectorPart vp = new VectorPart(new CuttingProperty(50, 100, 5000));
     vp.moveto(0, 0);
     vp.lineto(100, 0);
@@ -63,34 +94,18 @@ public class EpilogCutterTest
     vp.setFocus(300);
     vp.lineto(300, 0);
     vp.lineto(300, 100);
-    vp.lineto(400, 100);
-    vp.lineto(400, 0);
-    vp.lineto(300, 0);
-
+    vp.lineto(200, 100);
+    vp.lineto(200, 0);
+    System.out.println("Creating RP");
     RasterPart rp = new RasterPart(new EngravingProperty(80, 100));
-    GreyscaleRaster testimage = new GreyscaleRaster(){
-
-            public int getWidth() {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-
-            public int getGreyScale(int x, int y) {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-
-            public void setGreyScale(int x, int y, int grey) {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-
-            public int getHeight() {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-
-    };
-
-    rp.addImage(bwr, new Point(0, 200));
-
-    LaserJob job = new LaserJob("allparts", "666", "bla", 500, null, vp, null);
+    rp.addImage(new BlackWhiteRaster(new BufferedImageAdapter(getTestImage()), BlackWhiteRaster.DitherAlgorithm.FLOYD_STEINBERG), new Point(0, 200));
+    System.out.println("Creating R3dP");
+    Raster3dPart r3p = new Raster3dPart(new EngravingProperty(80, 100));
+    r3p.addImage(new BufferedImageAdapter(getTestImage()), new Point(0, 600));
+    System.out.println("Creating Job");
+    LaserJob job = new LaserJob("allparts", "666", "bla", 500, r3p, vp, rp);
+    System.out.println("Sending Job");
     instance.sendJob(job);
+    System.out.println("Done.");
   }
 }
