c68682024e508d452091a411a80c61f80e018465
Thomas Oster <thomas.oster@rwth-aachen.de>
2011-09-15 12:27:47 +0200

    more cleanup

--- test/com/t_oster/liblasercut/drivers/StartPointTest.java
+++ test/com/t_oster/liblasercut/drivers/StartPointTest.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of VisiCut.
+ * 
+ *     VisiCut is free software: you can redistribute it and/or modify
+ *     it under the terms of the Lesser GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *    VisiCut is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     Lesser GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with VisiCut.  If not, see <http://www.gnu.org/licenses/>.
+ **/
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -5,7 +21,6 @@
 
 package com.t_oster.liblasercut.drivers;
 
-import com.t_oster.liblasercut.drivers.EpilogCutter;
 import javax.swing.JOptionPane;
 import com.t_oster.liblasercut.LaserJob;
 import com.t_oster.liblasercut.VectorPart;
@@ -16,7 +31,6 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
