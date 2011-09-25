6fabb6f113e1a47f4c713e221d745696c63df3c7
Thomas Oster <thomas.oster@rwth-aachen.de>
2011-09-25 22:46:46 +0200

    Copyright Note added

--- src/com/t_oster/visicut/model/MaterialProfile.java
+++ src/com/t_oster/visicut/model/MaterialProfile.java
@@ -1,5 +1,6 @@
 /**
  * This file is part of VisiCut.
+ * Copyright (C) 2011 Thomas Oster <thomas.oster@rwth-aachen.de>
  * 
  *     VisiCut is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Lesser General Public License as published by
