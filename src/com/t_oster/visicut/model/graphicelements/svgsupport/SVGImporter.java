b5e4782c07020f0dc6be3224fcfb7cf3b03ac5e8
Thomas Oster <thomas.oster@rwth-aachen.de>
2011-10-07 19:28:59 +0200

    Illustrator SVG Resolution Fix

--- src/com/t_oster/visicut/model/graphicelements/svgsupport/SVGImporter.java
+++ src/com/t_oster/visicut/model/graphicelements/svgsupport/SVGImporter.java
@@ -32,12 +32,17 @@ import com.t_oster.visicut.model.graphicelements.GraphicObject;
 import com.t_oster.visicut.model.graphicelements.GraphicSet;
 import com.t_oster.visicut.model.graphicelements.ImportException;
 import java.awt.geom.AffineTransform;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.filechooser.FileFilter;
 
 /**
@@ -97,12 +102,63 @@ public class SVGImporter implements Importer
     }
   }
 
+  /*
+   * Tries to determine the Coordinate resolution in DPI.
+   * SVG default is 90, but AI generates 72??
+   * 
+   */
+  private int determineResolution(File f)
+  {
+    BufferedReader in = null;
+    int result = 90;
+    try
+    {
+      in = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+      try
+      {
+        for (int i = 0; i < 2; i++)
+        {
+          if (in.readLine().startsWith("<!-- Generator: Adobe Illustrator"))
+          {
+            result = 72;
+            break;
+          }
+        }
+        in.close();
+      }
+      catch (IOException ex)
+      {
+        Logger.getLogger(SVGImporter.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      return result;
+    }
+    catch (FileNotFoundException ex)
+    {
+      Logger.getLogger(SVGImporter.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    finally
+    {
+      try
+      {
+        in.close();
+      }
+      catch (IOException ex)
+      {
+        Logger.getLogger(SVGImporter.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    }
+    return result;
+  }
+
   @Override
   public GraphicSet importFile(File inputFile) throws ImportException
   {
     try
     {
-      return this.importFile(new FileInputStream(inputFile), inputFile.getName());
+      GraphicSet result = this.importFile(new FileInputStream(inputFile), inputFile.getName());
+      int dpi = this.determineResolution(inputFile);
+      result.setTransform(AffineTransform.getScaleInstance(500d / dpi, 500d / dpi));
+      return result;
     }
     catch (Exception ex)
     {
