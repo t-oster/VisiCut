4c6d3070a9ff881f8c104ac6afb12e73eeb75a40
Thomas Oster <thomas.oster@rwth-aachen.de>
2011-10-19 18:02:19 +0200

    Added Server script

--- tools/canonserver/capture.sh
+++ tools/canonserver/capture.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+#
+# This file is part of VisiCut.
+# Copyright (C) 2011 Thomas Oster <thomas.oster@rwth-aachen.de>
+# RWTH Aachen University - 52062 Aachen, Germany
+# 
+#     VisiCut is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU Lesser General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#    VisiCut is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Lesser General Public License for more details.
+# 
+#     You should have received a copy of the GNU Lesser General Public License
+#     along with VisiCut.  If not, see <http://www.gnu.org/licenses/>.
+#
 gphoto2 --capture-image-and-download >/dev/null 2>&1
 	echo -ne "HTTP/1.0 200 OK\r\n"
 	echo -ne "Content-Type: image/jpeg\r\n\r\n"
---
+++ tools/canonserver/server.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# This file is part of VisiCut.
+# Copyright (C) 2011 Thomas Oster <thomas.oster@rwth-aachen.de>
+# RWTH Aachen University - 52062 Aachen, Germany
+# 
+#     VisiCut is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU Lesser General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#    VisiCut is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Lesser General Public License for more details.
+# 
+#     You should have received a copy of the GNU Lesser General Public License
+#     along with VisiCut.  If not, see <http://www.gnu.org/licenses/>.
+#
+CAMERAS=$(gphoto2 --auto-detect|tail -n +3)
+if [ "$CAMERAS" == "" ]
+then
+	echo "Error: No camera detected"
+	exit 1
+else
+	echo "Detected Cameras:"
+	echo $CAMERAS
+fi
+echo "Starting Webserver. Listening on http://$(hostname):8080"
+shutdown()
+{
+	echo "Caught SIGTERM"
+	exit 0
+}
+trap 'shutdown' SIGTERM SIGINT
+while :
+do 
+	nc -e ./capture.sh -l -p 8080
+	echo "Request completed."
+done
