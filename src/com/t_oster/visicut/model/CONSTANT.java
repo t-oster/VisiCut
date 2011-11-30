156a77f6113a2be145d7ccda01e8303a575ea632
Axel <axel>
2011-11-30 15:40:13 +0100

    Added support for the Epilog HELIX Model. It runs by default with a higher resolution (1200dpi). The model and the dpi can now be set in the ManageLasercuttersDialog. Currently the desired resolution/dpi is used within the whole application (for the svg import, the display in the preview and the data that is sent to the lasercutter).In many cases the dpi setting of the LaserDevice is used but there are still a few lines where a constants are used. You can set these in the CONSTANT.java file.
    Tested only for svg import and cutting.
    
    Some Bugfixes (e.g. settings like bedheight etc. are now really saved for a newly created EpilogLaser)

---
+++ src/com/t_oster/visicut/model/CONSTANT.java
@@ -0,0 +1,13 @@
+package com.t_oster.visicut.model;
+public class CONSTANT {
+  public static final int     PROP_SVG_DPI                = 500;
+  public static final int     PROP_CUTTER_DPI             = 500;
+  public static final int     PROP_CUTTER_BEDHEIGHT       = 400;
+  public static final int     PROP_CUTTER_BEDWIDTH        = 600;
+  public static final int     PROP_CUTTER_MATERIALHEIGHT  = 300;
+  public static final int     PROP_CUTTER_MATERIALWIDTH   = 599;
+  //public static final String  PROP_CUTTER_STATICIP        = "137.226.56.228";
+  public static final String  PROP_CUTTER_STATICIP        = "192.168.100.4";
+  public static final int     PROP_PHOTO_DPI              = 500;
+  
+}
