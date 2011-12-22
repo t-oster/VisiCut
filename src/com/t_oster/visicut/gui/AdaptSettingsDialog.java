f0c82495540bbe8e006a491ce135871ec6c0770a
Thomas Oster <thomas.oster@rwth-aachen.de>
2011-12-22 19:54:42 +0100

    missing file header

--- src/com/t_oster/visicut/gui/AdaptSettingsDialog.java
+++ src/com/t_oster/visicut/gui/AdaptSettingsDialog.java
@@ -1,13 +1,22 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+ * This file is part of VisiCut.
+ * Copyright (C) 2011 Thomas Oster <thomas.oster@rwth-aachen.de>
+ * RWTH Aachen University - 52062 Aachen, Germany
+ * 
+ *     VisiCut is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *    VisiCut is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with VisiCut.  If not, see <http://www.gnu.org/licenses/>.
+ **/
 
-/*
- * AdaptSettingsDialog.java
- *
- * Created on 19.12.2011, 08:18:30
- */
 package com.t_oster.visicut.gui;
 
 import com.t_oster.liblasercut.LaserProperty;
@@ -23,7 +32,7 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author thommy
+ * @author Thomas Oster <thomas.oster@rwth-aachen.de>
  */
 public class AdaptSettingsDialog extends javax.swing.JDialog
 {
