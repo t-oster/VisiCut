05f9a9f77249fcf18c126fba11cc91b9ff0dbbc3
Axel <axel>
2011-12-27 18:14:21 +0100

    changed values in CONSTANT.java

--- src/com/t_oster/visicut/model/CONSTANT.java
+++ src/com/t_oster/visicut/model/CONSTANT.java
@@ -4,8 +4,8 @@ public class CONSTANT {
   public static final int     PROP_CUTTER_DPI             = 1200;
   public static final int     PROP_CUTTER_BEDHEIGHT       = 400;
   public static final int     PROP_CUTTER_BEDWIDTH        = 600;
-  public static final int     PROP_CUTTER_MATERIALHEIGHT  = 300;
-  public static final int     PROP_CUTTER_MATERIALWIDTH   = 599;
+  public static final int     PROP_CUTTER_MATERIALHEIGHT  = 400;
+  public static final int     PROP_CUTTER_MATERIALWIDTH   = 600;
   //public static final String  PROP_CUTTER_STATICIP        = "137.226.56.228";
   public static final String  PROP_CUTTER_STATICIP        = "192.168.100.4";
   public static final int     PROP_PHOTO_DPI              = 1200;
