830584049777012bce367b1b7d67903cb15969ae
Thomas Oster <thomas.oster@rwth-aachen.de>
2012-03-29 01:09:08 +0200

    updated pkgbuild for inkscape extension

--- tools/inkscape_extension/visicut_export.py
+++ tools/inkscape_extension/visicut_export.py
@@ -26,7 +26,7 @@ from lxml import etree
 from subprocess import Popen
 # Store the IDs of selected Elements
 elements=[]
-arguments=["--singleinstanceport", "65432"]
+arguments=["--singleinstanceport", "6543"]
 VISICUTBIN="visicut"
 
 for arg in sys.argv[1:]:
