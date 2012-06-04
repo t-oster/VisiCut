200c8b4d1367f76de336e009348028e656c5913c
Thomas Oster <thomas.oster@rwth-aachen.de>
2012-06-04 14:49:30 +0200

    More bullet proof Inkscape Script

--- tools/inkscape_extension/visicut_export.py
+++ tools/inkscape_extension/visicut_export.py
@@ -22,7 +22,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 '''
 
 import sys
-from lxml import etree
 SINGLEINSTANCEPORT=6543
 VISICUTBIN="visicut"
 # Store the IDs of selected Elements
@@ -50,22 +49,58 @@ def removeAllButThem(element, elements):
 			else:
 				keepSubtree = True
 		return keepSubtree
+
+def which(program):
+  import os
+  def is_exe(fpath):
+    return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+  fpath, fname = os.path.split(program)
+  if fpath:
+    if is_exe(program):
+      return program
+  else:
+    for path in os.environ["PATH"].split(os.pathsep):
+      exe_file = os.path.join(path, program)
+      if is_exe(exe_file):
+        return exe_file
+
+    return None
+
+
 # Strip SVG to only contain selected elements
-tree = etree.parse(filename)
-if len(elements) > 0:
-	removeAllButThem(tree.getroot(), elements)
-tree.write(filename+".svg")
+
+try:
+	from lxml import etree
+	tree = etree.parse(filename)
+	if len(elements) > 0:
+		removeAllButThem(tree.getroot(), elements)
+	tree.write(filename+".svg")
+except:
+	sys.stderr.write("Python-LXML not installed. Can only send complete SVG\n")
+	import shutil
+	shutil.copyfile(filename, filename+".svg")
 # Try to connect to running VisiCut instance
-import socket
-s=socket.socket()
-if s.connect(("localhost", SINGLEINSTANCEPORT)):
-	s.send(filename+".svg\n")
-	s.close()
-	sys.exit(0)
+try:
+  import socket
+  s=socket.socket()
+  s.connect(("localhost", SINGLEINSTANCEPORT))
+  s.send(filename+".svg\n")
+  s.close()
+  sys.exit(0)
+except SystemExit, e:
+    sys.exit(e)
+except:
+  pass
 # Try to start own VisiCut instance
-arguments=["--singleinstanceport", str(SINGLEINSTANCEPORT)]
-VISICUTBIN="visicut"
-from subprocess import Popen
-import daemonize
-daemonize.createDaemon()
-Popen([VISICUTBIN]+arguments+[filename+".svg"])
+try:
+  arguments=["--singleinstanceport", str(SINGLEINSTANCEPORT)]
+  if which(VISICUTBIN) == None:
+    sys.stderr.write("Error: Can't find VisiCut at '"+VISICUTBIN+"'. Please start VisiCut manually, add '"+VISICUTBIN+"' to the PATH Variable or change the VISICUTBIN variable in the Inkscape Extension.\n")
+  else:
+    from subprocess import Popen    
+    import daemonize
+    daemonize.createDaemon()
+    Popen([VISICUTBIN]+arguments+[filename+".svg"])
+except:
+  sys.stderr.write("Can not start VisiCut ("+str(sys.exc_info()[0])+"). Please start manually or change the VISICUTBIN variable in the Inkscape-Extension script\n")
