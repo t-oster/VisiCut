2335527b6fe4b49879d5c46988b6811a33b167c3
Thomas Oster <thomas.oster@rwth-aachen.de>
2012-10-30 14:13:13 +0100

    importers updated to mm

--- src/com/t_oster/visicut/model/graphicelements/jpgpngsupport/JPGPNGImporter.java
+++ src/com/t_oster/visicut/model/graphicelements/jpgpngsupport/JPGPNGImporter.java
@@ -2,17 +2,17 @@
  * This file is part of VisiCut.
  * Copyright (C) 2012 Thomas Oster <thomas.oster@rwth-aachen.de>
  * RWTH Aachen University - 52062 Aachen, Germany
- * 
+ *
  *     VisiCut is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Lesser General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
- * 
+ *
  *    VisiCut is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU Lesser General Public License for more details.
- * 
+ *
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with VisiCut.  If not, see <http://www.gnu.org/licenses/>.
  **/
@@ -22,6 +22,7 @@
  */
 package com.t_oster.visicut.model.graphicelements.jpgpngsupport;
 
+import com.t_oster.liblasercut.platform.Util;
 import com.t_oster.visicut.model.graphicelements.GraphicSet;
 import com.t_oster.visicut.model.graphicelements.ImportException;
 import com.t_oster.visicut.model.graphicelements.Importer;
@@ -44,7 +45,8 @@ public class JPGPNGImporter implements Importer
     {
       GraphicSet result = new GraphicSet();
       //TODO: Get Real Resolution
-      result.setBasicTransform(AffineTransform.getScaleInstance(500d/72, 500d/72));
+      double px2mm = Util.inch2mm(1d/72d);
+      result.setBasicTransform(AffineTransform.getScaleInstance(px2mm, px2mm));
       result.add(new JPGPNGImage(ImageIO.read(inputFile)));
       return result;
     }
@@ -71,8 +73,8 @@ public class JPGPNGImporter implements Importer
       {
         return "Rastergrafiken (jpg,png,bmp)";
       }
-      
+
     };
   }
-  
+
 }
