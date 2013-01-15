9269746e6588b35bbce448a03d0f1c2efb103a7b
Thomas Oster <thomas.oster@rwth-aachen.de>
2013-01-15 19:29:58 +0100

    VisiCam support

--- src/com/t_oster/visicut/misc/Helper.java
+++ src/com/t_oster/visicut/misc/Helper.java
@@ -32,9 +32,18 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
 import java.net.MalformedURLException;
+import java.net.NetworkInterface;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.URLDecoder;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.script.ScriptEngine;
@@ -48,6 +57,81 @@ import javax.script.ScriptEngineManager;
 public class Helper
 {
 
+  public static List<String> findVisiCamInstances()
+  {
+    List<String> result = new LinkedList<String>();
+    // Find the server using UDP broadcast
+    try {
+      //Open a random port to send the package
+      DatagramSocket c = new DatagramSocket();
+      c.setBroadcast(true);
+
+      byte[] sendData = "VisiCamDiscover".getBytes();
+
+      //Try the 255.255.255.255 first
+      try 
+      {
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("255.255.255.255"), 8888);
+        c.send(sendPacket);
+      } 
+      catch (Exception e) 
+      {
+      }
+
+      // Broadcast the message over all the network interfaces
+      Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+      while (interfaces.hasMoreElements()) 
+      {
+        NetworkInterface networkInterface = interfaces.nextElement();
+        if (networkInterface.isLoopback() || !networkInterface.isUp()) 
+        {
+          continue; // Don't want to broadcast to the loopback interface
+        }
+        for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) 
+        {
+          InetAddress broadcast = interfaceAddress.getBroadcast();
+          if (broadcast == null) {
+            continue;
+          }
+          // Send the broadcast package!
+          try 
+          {
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, broadcast, 8888);
+            c.send(sendPacket);
+          } 
+          catch (Exception e) 
+          {
+          }
+        }
+      }
+      //Wait for a response
+      byte[] recvBuf = new byte[15000];
+      c.setSoTimeout(3000);
+      while(true)
+      {
+        DatagramPacket receivePacket = new DatagramPacket(recvBuf, recvBuf.length);
+        try
+        {
+          c.receive(receivePacket);
+          //Check if the message is correct
+          String message = new String(receivePacket.getData()).trim();
+          //Close the port!
+          c.close();
+          if (message.startsWith("http")) {
+            result.add(message);
+          }
+        }
+        catch (SocketTimeoutException e)
+        {
+          break;
+        }
+      }
+    } catch (IOException ex) 
+    {
+    }
+    return result;
+  }
+  
   public static Double evaluateExpression(String expr)
   {
     expr = expr.replace(",", ".");
