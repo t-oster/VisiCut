d03e102f6da9acfcd7d5dec3aaff9b2536096b0c
Max Gaukler <development@maxgaukler.de>
2013-03-14 21:32:34 +0100

    MappingFilter: fix IndexOutOfBoundsException when attribute is not present

--- src/com/t_oster/visicut/model/mapping/MappingFilter.java
+++ src/com/t_oster/visicut/model/mapping/MappingFilter.java
@@ -100,6 +100,10 @@ public class MappingFilter
     if (value instanceof Number)
     {
       double number = ((Number) value).doubleValue();
+      if (e.getAttributeValues(attribute).isEmpty()) {
+        // object does not have the relevant attribute -> does not match (except if inverted)
+        return inverted;
+      }
       double other = ((Number) e.getAttributeValues(attribute).get(0)).doubleValue();
       result = attribute == null || (compare ? other <= number : other == number);
     }
