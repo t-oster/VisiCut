053f991dbcb7c5bdf7f453fc4843d7da7559129d
Thomas Oster <thomas.oster@rwth-aachen.de>
2013-05-18 08:15:02 +0200

    cosmetics

--- src/com/t_oster/visicut/gui/parameterpanel/ParameterPanel.java
+++ src/com/t_oster/visicut/gui/parameterpanel/ParameterPanel.java
@@ -1,26 +1,34 @@
+/**
+ * This file is part of VisiCut.
+ * Copyright (C) 2011 - 2013 Thomas Oster <thomas.oster@rwth-aachen.de>
+ * RWTH Aachen University - 52062 Aachen, Germany
+ *
+ *     VisiCut is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     VisiCut is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with VisiCut.  If not, see <http://www.gnu.org/licenses/>.
+ **/
 package com.t_oster.visicut.gui.parameterpanel;
 
 import com.t_oster.uicomponents.parameter.Parameter;
 import com.t_oster.visicut.VisicutModel;
 import com.t_oster.visicut.gui.MainView;
 import com.t_oster.visicut.model.PlfPart;
-import com.t_oster.visicut.model.graphicelements.ImportException;
-import com.t_oster.visicut.model.graphicelements.psvgsupport.PSVGImporter;
 import com.t_oster.uicomponents.parameter.ParameterTableModel;
 import com.t_oster.visicut.model.graphicelements.psvgsupport.ParametricPlfPart;
-import java.awt.geom.AffineTransform;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
-import javax.xml.parsers.ParserConfigurationException;
-import org.xml.sax.SAXException;
 
 /**
  *
