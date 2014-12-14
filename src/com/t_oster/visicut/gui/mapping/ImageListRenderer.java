b84817f34c238809008b327aa8071688ea6bf173
Sven Jung <foreverJungzzz@googlemail.com>
2014-12-14 20:59:09 +0100

    added two list mappings: 1. MapListModel: Takes an map and provides the keys to a list. Class needs Map<String, T>. String will be displayed, the generic type T is never accessed but is needed to work on abstract maps. 2. ImageListRenderer: Takes an Map<String, ImageIcon> and displays the ImageIcons for each ListEntry with value String.

---
+++ src/com/t_oster/visicut/gui/mapping/ImageListRenderer.java
@@ -0,0 +1,44 @@
+package com.t_oster.visicut.gui.mapping;
+
+import java.awt.Component;
+import java.awt.Font;
+import java.util.Map;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JList;
+
+/**
+ * This class is for rendering images at the left side of each list element.
+ * @param Map<String, ImageIcon> is a mapping of String label of list elements to images to display.
+ * Suggestion would be to use the same map for this Renderer and the List and create the list with List(map.keySet().toArray()) 
+ * or even better use the corresponding MapListModel.
+ * @author Sven
+ */
+public class ImageListRenderer extends DefaultListCellRenderer {
+
+  Font font = new Font("helvitica", Font.BOLD, 24);
+  Map<String, ImageIcon> model = null;
+
+  public ImageListRenderer(Map<String, ImageIcon> mapModel){
+    this.model = mapModel;
+  }
+
+  @Override
+  public Component getListCellRendererComponent(
+          JList list, Object value, int index,
+          boolean isSelected, boolean cellHasFocus) {
+
+      JLabel label = (JLabel) super.getListCellRendererComponent(
+              list, value, index, isSelected, cellHasFocus);
+      label.setIcon(model.get((String) value));
+      label.setHorizontalTextPosition(JLabel.RIGHT);
+      label.setFont(font);
+      return label;
+  }
+
+  @Override
+  public void setFont(Font font){
+    this.font = font;
+  }
+}
