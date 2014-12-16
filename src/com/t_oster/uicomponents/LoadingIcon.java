eb75323d62733a29d709b8066890602f29affc74
Sven Jung <foreverJungzzz@googlemail.com>
2014-12-16 20:39:53 +0100

    Added LoadingIcon class similar to PlatformIcon.

---
+++ src/com/t_oster/uicomponents/LoadingIcon.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.t_oster.uicomponents;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Sven
+ */
+public class LoadingIcon{
+  public static final String CIRCLEBALL = "circleball";
+
+  public static ImageIcon get(String type){
+    try
+    {
+      return new ImageIcon(LoadingIcon.class.getResource("resources/"+type+".gif"));
+    }
+    catch (Exception ex)
+    {
+      System.err.println("Error loading "+"resources/"+type+".gif");
+      ex.printStackTrace();
+    }
+    return null;
+  }
+  
+}
+
