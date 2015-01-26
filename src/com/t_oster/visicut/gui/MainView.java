6fdb15b62f355e593d23f90b0867406d30c0c965
Thomas Oster <thomas.oster@rwth-aachen.de>
2015-01-26 19:42:04 +0100

    Add Settings URL for Leeds Hackspace. Fixes #234

--- src/com/t_oster/visicut/gui/MainView.java
+++ src/com/t_oster/visicut/gui/MainView.java
@@ -2508,6 +2508,7 @@ private void jmDownloadSettingsActionPerformed(java.awt.event.ActionEvent evt) {
   choices.put("Germany, Erlangen: FAU FabLab", "https://github.com/fau-fablab/visicut-settings/archive/master.zip");
   choices.put("Netherlands, Amersfoort: FabLab", "https://github.com/Fablab-Amersfoort/visicut-settings/archive/master.zip");
   choices.put("United Kingdom, Manchester: Hackspace", "https://github.com/hacmanchester/visicut-settings/archive/master.zip");
+  choices.put("United Kingdom, Leeds: Hackspace", "https://github.com/pbrook/visicut-settings/archive/master.zip");
   choices.put(bundle.getString("DOWNLOAD_NOT_IN_LIST"),"__HELP__");
   String s = (String)JOptionPane.showInputDialog(this, bundle.getString("DOWNLOAD_SETTINGS_INFO"), null, JOptionPane.PLAIN_MESSAGE, null, choices.keySet().toArray(), choices.keySet().toArray()[0]);
   if ((s==null) || (s.length() == 0)) {
