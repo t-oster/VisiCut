8ed859fab5407949c8984999c6e4cc42bf2f34c0
TheAssassin <theassassin@assassinate-you.net>
2020-02-19 18:21:29 +0100

    Add Docker AppImage build scripts

---
+++ distribute/linux/build-appimage-in-docker.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+set -e
+set -x
+
+# calculate path to project root relative to this file
+project_root=$(readlink -f $(dirname "$0"))/../..
+
+# change to project root directory
+cd "$project_root"
+
+# build Docker image to cache dependencies between builds
+image=visicut-appimage-build
+dockerfile=distribute/linux/Dockerfile.build-appimage
+docker build -t "$image" -f "$dockerfile" $(dirname "$dockerfile")
+
+docker run --rm -i -v "$project_root":/ws "$image" sh -x <<EOF
+cd /ws
+export APPIMAGE_EXTRACT_AND_RUN=1
+appimagecraft
+EOF
