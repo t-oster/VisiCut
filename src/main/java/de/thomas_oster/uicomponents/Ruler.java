t-oster/VisiCut

commit c0606a04246068b728fcc1bb927b250d4240f8c7

Fix numbers in PreviewPanel ruler for very high zoom

src/main/java/de/thomas_oster/uicomponents/Ruler.java
@@ -76,9 +76,22 @@ public class Ruler extends JPanel implements PropertyChangeListener, ComponentLi
         g.drawLine((int) (dst*mm), (drawText ? 5 : 6)*size/8, (int) (dst*mm), size);
         if (drawText && mm != 0)
         {
+          if (mm < g.getClipBounds().getMinX() / dst)
+          {
+            // Workaround arithmetic overflow in Java.Graphics2D
+            // (drawing on very large coordinates is not correctly clipped.)
+            // -> Skip drawing if the path is outside of the clipping area.
+            continue;
+          }
+
           String txt = ""+mm/10;
           int w = g.getFontMetrics().stringWidth(txt);
           g.drawString(txt, (int) (dst*mm)-w/2, size/2);
+          if (dst * mm  > g.getClipBounds().getMaxX() + w)
+          {
+            // stop drawing at the right border.
+            break;
+          }
         }
       }
     }
@@ -95,6 +108,13 @@ public class Ruler extends JPanel implements PropertyChangeListener, ComponentLi
       double dst = mm2px.getScaleY();
       for (int mm = 0; mm < target.getAreaSize().y; mm+= dst > 5 ? 1 : dst > 2 ? 5 : 10)
       {
+          if (mm < g.getClipBounds().getMinY() / dst)
+          {
+            // Workaround arithmetic overflow in Java.Graphics2D
+            // (drawing on very large coordinates is not correctly clipped.)
+            // -> Skip drawing if the path is outside of the clipping area.
+            continue;
+          }
         boolean drawText = mm % (dst > 3 ? 10 : 100) == 0;
         double y = (dst*mm);
         if (originBottom) {
@@ -105,6 +125,11 @@ public class Ruler extends JPanel implements PropertyChangeListener, ComponentLi
         {
           int h = g.getFontMetrics().getHeight();
           g.drawString(""+mm/10, 0, (int) y+h/2);
+          if (dst * mm  > g.getClipBounds().getMaxY() + h)
+          {
+            // stop drawing at the bottom border.
+            break;
+          }
         }
       }
     }

commit ee01263f01cc5d5bc854f398573646a2339b3679

Fix drawing of Y axis ruler
Fixes https://github.com/t-oster/VisiCut/issues/608
Regression from c0606a04246068b728fcc1bb927b250d4240f8c7

src/main/java/de/thomas_oster/uicomponents/Ruler.java
@@ -108,28 +108,23 @@ public class Ruler extends JPanel implements PropertyChangeListener, ComponentLi
       double dst = mm2px.getScaleY();
       for (int mm = 0; mm < target.getAreaSize().y; mm+= dst > 5 ? 1 : dst > 2 ? 5 : 10)
       {
-          if (mm < g.getClipBounds().getMinY() / dst)
-          {
-            // Workaround arithmetic overflow in Java.Graphics2D
-            // (drawing on very large coordinates is not correctly clipped.)
-            // -> Skip drawing if the path is outside of the clipping area.
-            continue;
-          }
         boolean drawText = mm % (dst > 3 ? 10 : 100) == 0;
         double y = (dst*mm);
         if (originBottom) {
           y = dst*bedHeight - y;
+        }
+        if (y < g.getClipBounds().getMinY() || y > g.getClipBounds().getMaxY())
+        {
+          // Workaround arithmetic overflow in Java.Graphics2D
+          // (drawing on very large coordinates is not correctly clipped.)
+          // -> Skip drawing if the path is outside of the clipping area.
+          continue;
         }
         g.drawLine((drawText ? 5 : 6)*size/8, (int) y, size, (int) y);
         if (drawText && mm != 0)
         {
           int h = g.getFontMetrics().getHeight();
           g.drawString(""+mm/10, 0, (int) y+h/2);
-          if (dst * mm  > g.getClipBounds().getMaxY() + h)
-          {
-            // stop drawing at the bottom border.
-            break;
-          }
         }
       }
     }