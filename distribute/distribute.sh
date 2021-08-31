ebb331818fba3a341e8bff4023ef5fc8ba8619fc
Max Gaukler <development@maxgaukler.de>
2021-08-31 17:33:24 +0200

    Always use Java 11 on Linux
    Workaround for:
    https://github.com/t-oster/VisiCut/issues/614
    https://github.com/t-oster/VisiCut/issues/613

--- distribute/distribute.sh
+++ distribute/distribute.sh
@@ -165,7 +165,7 @@ then
   # hide doc directory from checkinstall
   # mv doc doctmp
   test -f /usr/bin/visicut && { echo "error: please first uninstall visicut"; exit 1; }
-  fakeroot checkinstall --fstrans --reset-uid --type debian --install=no -y --pkgname visicut --pkgversion $VERSION --arch all --pkglicense LGPL --pkggroup other --pkgsource "http://visicut.org" --pkgaltsource "https://github.com/t-oster/VisiCut" --pakdir distribute/ --maintainer "'Thomas Oster <thomas.oster@rwth-aachen.de>'" --requires "bash,java-runtime,potrace" make install -e PREFIX=/usr > /dev/null || { echo "error"; exit 1; }
+  fakeroot checkinstall --fstrans --reset-uid --type debian --install=no -y --pkgname visicut --pkgversion $VERSION --arch all --pkglicense LGPL --pkggroup other --pkgsource "http://visicut.org" --pkgaltsource "https://github.com/t-oster/VisiCut" --pakdir distribute/ --maintainer "'Thomas Oster <thomas.oster@rwth-aachen.de>'" --requires "bash,openjdk-11-jre,potrace" make install -e PREFIX=/usr > /dev/null || { echo "error"; exit 1; }
   rm -rf *-pak
   # mv doctmp doc
   popd > /dev/null
@@ -189,5 +189,5 @@ then
 fi
 
 echo "Cleaning up..."
-rm -rf visicut  
+rm -rf visicut
 echo "done."
