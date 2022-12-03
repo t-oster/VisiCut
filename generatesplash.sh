bece97bc95636d5acc17665eecd7b243575df35f
Max Gaukler <development@maxgaukler.de>
2022-12-03 14:52:17 +0100

    New splash screen
    https://github.com/t-oster/VisiCut/issues/601

--- generatesplash.sh
+++ generatesplash.sh
@@ -19,10 +19,10 @@ echo "Version is: $VERSION"
 echo "Generating SVG"
 cat splashsource.svg|sed s#insert#$VERSION#g# > splash.svg
 echo "Converting to png"
-rsvg-convert -w 404 -h 304 splash.svg > src/main/resources/de/thomas_oster/visicut/gui/resources/splash.png
+rsvg-convert -w 514 -h 444 splash.svg > src/main/resources/de/thomas_oster/visicut/gui/resources/splash.png
 # high-dpi variants (see https://docs.oracle.com/javase/10/docs/api/java/awt/SplashScreen.html )
-rsvg-convert -w 808 -h 608 splash.svg > src/main/resources/de/thomas_oster/visicut/gui/resources/splash@2x.png
-rsvg-convert -w 1212 -h 912 splash.svg > src/main/resources/de/thomas_oster/visicut/gui/resources/splash@3x.png
+rsvg-convert -w 1028 -h 888 splash.svg > src/main/resources/de/thomas_oster/visicut/gui/resources/splash@2x.png
+rsvg-convert -w 1542 -h 1332 splash.svg > src/main/resources/de/thomas_oster/visicut/gui/resources/splash@3x.png
 echo "cleaning..."
 rm splash.svg
 echo "done."
