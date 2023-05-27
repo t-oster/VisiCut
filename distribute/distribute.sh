544ac7f07da2e54f93497d315efa79558efdc686
TheAssassin <theassassin@assassinate-you.net>
2023-05-27 03:40:34 +0200

    Rewrite distribute.sh completely to make it usable

--- distribute/distribute.sh
+++ distribute/distribute.sh
@@ -1,193 +1,291 @@
-#!/bin/bash
-set -e # exit 1 if any command fails
-set -o pipefail
-cd "$(dirname "$0")"
-# set -x # uncomment for debugging
-if [ "$1" == "--nocompile" ]
-then
-   COMPILE=0
-else
-   COMPILE=1
-fi
-echo "Determining Version (may be overridden with environment variable VERSION):"
-# VisiCutBuilder writes the setting in that file:
-VERSION=${VERSION:-$(cat ../src/main/resources/de/thomas_oster/visicut/gui/resources/VisicutApp.properties |grep Application.version)}
-# remove "Application.version =   " prefix which comes from the VisiCutApp.properties file
-VERSION=${VERSION#*=}
-VERSION=${VERSION// /}
-# normally running ./distribute.sh doesn't write the above file. Then, fall back to what git describe tells us
-VERSION=${VERSION:-$(git describe --tags || echo unknown)+devel}
-echo "Version is: \"$VERSION\""
-if [ "$COMPILE" == 1 ]
-then
-	echo "Building jar..."
-	cd ..
-	make clean > /dev/null
-	VERSION="$VERSION" make > /dev/null || { echo "Compilation failed. Please run 'make' to see what failed."; exit 1; }
-	cd distribute
+#! /bin/bash
+
+set -euo pipefail
+
+# small optimization: if a RAM disk is available, use it for building
+# saving writes on an SSD will extend its life, after all!
+if [[ "${TMPDIR:-}" == "" ]] && [[ -d /dev/shm ]]; then
+    export TMPDIR=/dev/shm
 fi
 
-echo "Removing leftover files"
-rm -rf visicut
-
-echo "Copying content..."
-mkdir visicut
-cp -r ../target/visicut*full.jar visicut/Visicut.jar
-cp -r files/* visicut/
-cp ../README.md visicut/README
-cp ../COPYING.LESSER visicut/
-cp ../LICENSE visicut/
-chmod +x visicut/*.jar
-chmod +x visicut/VisiCut.*
-mkdir -p visicut/inkscape_extension
-cp ../tools/inkscape_extension/*.py visicut/inkscape_extension/
-cp ../tools/inkscape_extension/*.inx visicut/inkscape_extension/
-mkdir -p visicut/illustrator_script
-cp ../tools/illustrator_script/*.scpt visicut/illustrator_script/
-
-# check_sha256 <filename> <hash>
-# -> returns 0 if file exists and its hash is correct, 1 otherwise.
-function check_sha256() {
-    test -e "$1" || return 1
-    sha256sum "$1" | grep -q "$2" || { echo "SHA256 hash of $1 does not match. Expected: $2, Got:"; sha256sum $1; return 1; }
-    return 0
+log() {
+    [[ -t 0 ]] && tput setaf 2
+    [[ -t 0 ]] && tput bold
+    echo "== $* =="
+    [[ -t 0 ]] && tput sgr0
 }
 
-# URL and hash of the OpenJRE ZIP file for Windows.
-# You can override this with environment variables.
-# The distribution by Oracle has evil license terms, so we use the OpenJDK JRE from https://adoptium.net/
-WINDOWS_JRE_URL=${WINDOWS_JRE_URL:-"https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.5%2B8/OpenJDK17U-jre_x64_windows_hotspot_17.0.5_8.zip"}
-WINDOWS_JRE_SHA256=${WINDOWS_JRE_SHA256:-"d25a2d44c1bd9c57d49c5e98de274cd40970ab057fe304b52eb459de4ee5d8a5"}
+# targets to be built need to be passed on the commandline
+# if no targets are provided, we display a help text
+if [[ "${1:-}" == "" ]]; then
+    log "Usage: [env BUILD=1] $0 <targets>"
+    echo
+    log "Available targets:"
+    log "    - zip"
+    log "    - windows-nsis"
+    log "    - macos-bundle"
+    log "    - linux-appimage"
+    log "    - linux-checkinstall"
+    log "    - linux-archlinux"
+    echo
+    log "Available environment variables:"
+    log "    - \$BUILD=[...]: if set to any string, $0 will build VisiCut's JAR first (not set by default)"
+    exit 2
+fi
 
-if which makensis > /dev/null
-then
-	mkdir -p cache
-	echo "Downloading OpenJRE for Windows (may be overridden with WINDOWS_JRE_URL and WINDOWS_JRE_SHA256)"
-	# download JRE if not existing or wrong file contents
-    check_sha256 cache/jre.zip $WINDOWS_JRE_SHA256 || wget "$WINDOWS_JRE_URL" -O cache/jre.zip
-    # check if downloaded JRE is correct (to exclude incomplete download)
-    check_sha256 cache/jre.zip $WINDOWS_JRE_SHA256 || exit 1
-    echo "Building Windows launcher and installer"
-	# Copy files to wintmp/
-	rm -rf wintmp
-	mkdir wintmp
-	cp -r windows/* wintmp/
-	[ -d wintmp/stream ] || mkdir wintmp/stream
-	cp -r visicut/* wintmp/stream/
-	cp ../src/main/resources/de/thomas_oster/visicut/gui/resources/splash*.png wintmp/stream
-	cp ../tools/inkscape_extension/* wintmp/
+# we move all build artifacts back to the script's original working directory
+old_cwd="$(readlink -f "$PWD")"
 
-	# build setup.exe installer
-	# and VisiCut.exe launcher executable
-	cat windows/installer.nsi|sed s#VISICUTVERSION#"$VERSION"#g > wintmp/installer.nsi
-	pushd wintmp > /dev/null
-	# Unpack JRE. We assume that this creates a subfolder with "jre" in its name
-	unzip -q ../cache/jre.zip
-    mv *jre*/ stream/jre/
-	test -e stream/jre/bin/java.exe || { echo "Cannot find java.exe in JRE ZIP file"; exit 1; }
-	test -e stream/jre/legal/java.base/LICENSE || { echo "Cannot find license information in JRE ZIP file"; exit 1; }
-	# Build license text for Windows installer
-    {
-        cat ../../LICENSE
-        echo "Java Runtime for Windows:"
-        echo -e "- OpenJRE (GPLv2 with Classpath exception). License details follow:"
-        find stream/jre/legal/ -type f -print -exec cat '{}' ';'
-    }  > stream/license-with-jre.txt
-	makensis launcher.nsi > /dev/null || exit 1 # build VisiCut.exe
-	cp VisiCut.exe ../visicut/ # copy VisiCut.exe so that it is included in the platform independent ZIP
-	mv VisiCut.exe ./stream/
-	makensis installer.nsi > /dev/null || exit 1 # build setup.exe
-	popd
-	mv wintmp/setup.exe VisiCut-$VERSION-Windows-Installer.exe || exit 1
-	zip VisiCut-$VERSION-Windows-Installer.zip VisiCut-$VERSION-Windows-Installer.exe	# for github upload
+# make it easier to work from the script
+distribute_dir="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
 
-	# cleanup
-	rm -rf wintmp
+# we assume the distribute directory is a direct subdirectory of the project root directory (i.e., the Git repository)
+project_root_dir="$(readlink -f "$distribute_dir"/..)"
+
+if [[ "${VERSION:-}" != "" ]]; then
+    log "Using user-provided version \$VERSION=$VERSION"
 else
-	echo "NSIS not found. The resulting ZIP will be missing the Visicut.exe launcher for Windows."
-	echo "Not building Windows installer."
+    properties="$project_root_dir"/src/main/resources/de/thomas_oster/visicut/gui/resources/VisicutApp.properties
+    VERSION="$(grep Application.version < "$properties" | cut -d= -f2 | tr -d ' ')"
+
+    # usually, distribute.sh doesn't create the properties file, so we implement a Git based fallback
+    if [[ "$VERSION" != "" ]]; then
+        log "Using properties-provided version $VERSION"
+    else
+        VERSION="$(git describe --tags || echo unknown)+devel"
+    fi
 fi
 
-echo "Compressing content..."
-[ -f VisiCut-$VERSION.zip ] && rm VisiCut-$VERSION.zip
-zip -r VisiCut-$VERSION.zip visicut/  > /dev/null || exit 1
+export VERSION
 
-echo ""
-echo "****************************************************************"
-echo "Mac OS Version: Building the Mac OS Version should work on all platforms"
-echo "Build Mac OS Version (Y/n)?"
-# URL and hash of the OpenJRE ZIP file for OSX.
-# You can override this with environment variables.
-# The distribution by Oracle has evil license terms, so we use the OpenJDK JRE from https://adoptopenjdk.net/
-OSX_JRE_URL=${OSX_JRE_URL:-"https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.5%2B10/OpenJDK11U-jre_x64_mac_hotspot_11.0.5_10.tar.gz"}
-OSX_JRE_SHA256=${OSX_JRE_SHA256:-"dfd212023321ebb41bce8cced15b4668001e86ecff6bffdd4f2591ccaae41566"}
-read answer || true
-if [ "$answer" != "n" ]
-then
-  echo "Creating Mac OS Bundle"
-  [ -d VisiCut.app ] && rm -rf VisiCut.app
-  cp -r "mac/VisiCut.app" .
-  mkdir -p "VisiCut.app/Contents/Resources/Java"
-  cp -r visicut/* "VisiCut.app/Contents/Resources/Java/"
-  mkdir "VisiCut.app/Contents/Java"
-  mv "VisiCut.app/Contents/Resources/Java/Visicut.jar" "VisiCut.app/Contents/Java/"
-  cp ../src/main/resources/de/thomas_oster/visicut/gui/resources/splash*.png "VisiCut.app/Contents/Resources/Java"
-  echo "Updating Bundle Info"
-  cp "VisiCut.app/Contents/Info.plist" .
-  cat Info.plist|sed s#VISICUTVERSION#"$VERSION"#g > VisiCut.app/Contents/Info.plist
-  rm Info.plist
-  mkdir -p mac/jre
-  echo "Downloading OpenJRE for OSX (may be overridden with OSX_JRE_URL and OSX_JRE_SHA256)"
-  # download JRE if not existing or wrong file contents
-  check_sha256 mac/jre/jre.tar.gz $OSX_JRE_SHA256 || wget "$OSX_JRE_URL" -O mac/jre/jre.tar.gz
-  # check if downloaded JRE is correct (to exclude incomplete download)
-  check_sha256 mac/jre/jre.tar.gz $OSX_JRE_SHA256 || exit 1
-  echo "Inserting JRE into the app bundle..:"
-  mkdir -p VisiCut.app/Contents/Plugins
-  tar -xf mac/jre/jre.tar.gz -C VisiCut.app/Contents/Plugins
-  mv VisiCut.app/Contents/Plugins/jdk-11.0.5+10-jre VisiCut.app/Contents/Plugins/JRE
-  echo "Compressing Mac OS Bundle"
-  rm -rf VisiCutMac-$VERSION.zip
-  zip -r VisiCutMac-$VERSION.zip VisiCut.app > /dev/null || exit 1
+if [ "${BUILD:-}" != "" ]; then
+    log "Building VisiCut JAR"
+    # TODO: make out-of-source builds possible
+	pushd "$project_root_dir"
+	make clean jar
+	popd
 fi
 
-echo "Dir:$(pwd)"
-echo "****************************************************************"
-echo "Ubuntu Version: For Building you must have checkinstall and dpkg"
-echo "and no VisiCut installation may be installed."
-echo "Build Ubuntu Version (Y/n)?"
-read answer || true
-if [ "$answer" != "n" ]
-then
-  pushd . > /dev/null
-  cp linux/*-pak ../
-  cd ..
-  # hide doc directory from checkinstall
-  # mv doc doctmp
-  test -f /usr/bin/visicut && { echo "error: please first uninstall visicut"; exit 1; }
-  fakeroot checkinstall --fstrans --reset-uid --type debian --install=no -y --pkgname visicut --pkgversion $VERSION --arch all --pkglicense LGPL --pkggroup other --pkgsource "http://visicut.org" --pkgaltsource "https://github.com/t-oster/VisiCut" --pakdir distribute/ --maintainer "'Thomas Oster <thomas.oster@rwth-aachen.de>'" --requires 'bash,openjdk-11-jre\|openjdk-17-jre,potrace' make install -e PREFIX=/usr > /dev/null || { echo "error"; exit 1; }
-  rm -rf *-pak
-  # mv doctmp doc
-  popd > /dev/null
+# we "ab"use the deployment directory to create the "visicut directory"
+# this directory contains all of VisiCut's binaries and resources
+# consider it a semi-portable bundle (a JVM is needed to make it complete)
+# the resulting directory can then be put into various types of packages
+visicut_dir="$(readlink -f "$distribute_dir"/visicut)"
+
+if [[ -d "$visicut_dir" ]]; then
+    log "Cleaning up old visicut directory in $visicut_dir"
+    rm -r "$visicut_dir"
 fi
 
-echo "Dir: $(pwd)"
-echo "****************************************************************"
-echo "Arch Linux Version: For Building you must have pacman installed."
-echo "Build Arch Linux Version (Y/n)?"
-read answer || true
-if [ "$answer" != "n" ]
-then
-  cd linux
-  ARCHVERSION=$(echo $VERSION|sed "s#-#_#g")
-  cat PKGBUILD | sed "s#pkgver=VERSION#pkgver=$ARCHVERSION#g" > PKGBUILD-tmp
-  makepkg -p PKGBUILD-tmp > /dev/null || exit 1
-  mv *.pkg.tar.xz ../
-  echo "Cleaning up..."
-  rm -rf src pkg PKGBUILD-tmp
-  cd ..
-fi
+log "Creating visicut directory in $visicut_dir"
 
-echo "Cleaning up..."
-rm -rf visicut
-echo "done."
+mkdir "$visicut_dir"
+
+cp -v "$project_root_dir"/target/visicut-*-full.jar "$visicut_dir"/Visicut.jar
+cp -R -v "$distribute_dir"/files/* "$visicut_dir"/
+chmod +x "$visicut_dir"/{*.jar,VisiCut.*}
+
+cp -v "$project_root_dir"/README.md "$visicut_dir"/README
+cp -v "$project_root_dir"/{COPYING.LESSER,LICENSE} "$visicut_dir"/
+
+mkdir -p "$visicut_dir"/{inkscape_extension,illustrator_script}
+cp -v "$project_root_dir"/tools/inkscape_extension/{*.py,*.inx} "$visicut_dir"/inkscape_extension/
+cp -v "$project_root_dir"/tools/illustrator_script/*.scpt "$visicut_dir"/illustrator_script/
+
+# now that the visicut directory has been set up, we can perform platform-specific bundling
+for target in "$@"; do
+    log "Building for target $target"
+
+    build_dir="$(mktemp -d -t visicut-build-$target-XXXXXX)"
+    cleanup() {
+        [[ -d "$build_dir" ]] && rm -rf "${build_dir:?}"/
+    }
+    trap cleanup EXIT
+
+    download_and_extract_jdk() {
+        local url="$1"
+        local hash="$2"
+
+        pushd "$build_dir"
+
+        wget --content-disposition "$url"
+        downloaded_file="$(find . -iname '*.zip' -or -iname '*.tar*' -type f | head -n1)"
+        echo "$hash  $downloaded_file" | sha256sum -c
+
+        # -bb1 displays a list of extracted files
+        if [[ "$downloaded_file" == *.zip ]]; then
+            unzip ./*.zip
+        elif [[ "$downloaded_file" == ./*.tar* ]]; then
+            tar -xvf ./*.tar*
+        else
+            # should never happen
+            log "Unknown archive type"
+            exit 4
+        fi
+
+        mv ./*jre/ jre
+
+        popd
+    }
+
+    build_windows_launcher() {
+        # build launcher
+        pushd "$build_dir"
+        # need to copy the NSIS files to the build dir, since relative paths are relative to the config directory
+        cp -Rv "$distribute_dir"/windows "$build_dir"/windows-launcher
+        pushd windows-launcher
+        makensis launcher.nsi
+        popd
+        mv -v windows-launcher/VisiCut.exe visicut/
+        rm -rf windows-launcher/
+        popd
+    }
+
+    case "$target" in
+        zip)
+            pushd "$build_dir"
+            cp -Rv "$visicut_dir" visicut
+            build_windows_launcher
+            zip -r visicut.zip visicut
+            mv visicut.zip "$old_cwd"
+            popd
+            ;;
+
+        windows-nsis)
+            install -D "$project_root_dir"/src/main/resources/de/thomas_oster/visicut/gui/resources/splash*.png "$build_dir"/
+
+            jre_url="https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.5%2B8/OpenJDK17U-jre_x64_windows_hotspot_17.0.5_8.zip"
+            jre_hash="d25a2d44c1bd9c57d49c5e98de274cd40970ab057fe304b52eb459de4ee5d8a5"
+            download_and_extract_jdk "$jre_url" "$jre_hash"
+
+            # validate JRE
+            test -e "$build_dir"/jre/bin/java.exe
+            test -e "$build_dir"/jre/legal/java.base/LICENSE
+
+            # set up installer-specific license information
+            cat >"$build_dir"/license-with-jre.txt <<EOF
+"$(cat "$project_root_dir"/LICENSE)"
+
+Java runtime for Windows: OpenJRE \(GPLv2 with Classpath exception\)
+"$(find "$build_dir"/jre/legal -type f -exec cat '{}' \;)"
+EOF
+
+            # copy visicut directory for packaging
+            cp -R "$visicut_dir"/ "$build_dir"/visicut
+
+            # place JRE in visicut dir
+            mv "$build_dir"/jre "$build_dir"/visicut/jre
+
+            build_windows_launcher
+
+            # build the installer
+            pushd "$build_dir"
+            # need to copy the NSIS files to the build dir, since relative paths are relative to the config directory
+            cp -Rv "$distribute_dir"/windows/* "$build_dir"/
+            makensis installer.nsi
+            popd
+
+            filename_prefix="VisiCut-$VERSION-Windows-Installer"
+            mv "$build_dir"/setup.exe "$filename_prefix".exe
+            ;;
+
+        macos-bundle)
+            jre_url="https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.5%2B10/OpenJDK11U-jre_x64_mac_hotspot_11.0.5_10.tar.gz"
+            jre_hash="dfd212023321ebb41bce8cced15b4668001e86ecff6bffdd4f2591ccaae41566"
+            download_and_extract_jdk "$jre_url" "$jre_hash"
+
+            # prepare bundle directory
+            pushd "$build_dir"
+
+            # copy app template directory
+            cp -R "$distribute_dir"/mac/VisiCut.app .
+
+            # copy visicut directory contents into macOS style location
+            mkdir -p VisiCut.app/Contents/Resources/Java/
+            cp -Rv "$visicut_dir"/* VisiCut.app/Contents/Resources/Java/
+
+            # however, the JAR needs to be put into another location
+            mkdir -p VisiCut.app/Contents/Java
+            mv VisiCut.app/Contents/Resources/Java/Visicut.jar VisiCut.app/Contents/Java/
+
+            cp "$project_root_dir"/src/main/resources/de/thomas_oster/visicut/gui/resources/splash*.png VisiCut.app/Contents/Resources/Java
+
+            # update version
+            # unfortunately, this is the most elegant way we can use the file as a template
+            sed -i s#VISICUTVERSION#"$VERSION"#g VisiCut.app/Contents/Info.plist
+
+            # deploy jre
+            mkdir -p VisiCut.app/Contents/Plugins/
+            mv jre/* VisiCut.app/Contents/Plugins/
+
+            # create bundle
+            zip -r bundle.zip VisiCut.app/
+
+            popd
+
+            filename_prefix="VisiCutMac-$VERSION"
+            mv "$build_dir"/bundle.zip "$filename_prefix".zip
+            ;;
+
+        linux-appimage)
+            pushd "$build_dir"
+
+            # appimagecraft needs a jre and visicut dir next to its config file, so let's do that!
+            jre_url="https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.5%2B8/OpenJDK17U-jre_x64_linux_hotspot_17.0.5_8.tar.gz"
+            jre_hash="11326464a14b63e6328d1d2088a23fb559c0e36b3f380e4c1f8dcbe160a8b95e"
+            download_and_extract_jdk "$jre_url" "$jre_hash"
+
+            # also, some desktop integration stuff
+            cp "$distribute_dir"/linux/VisiCut.desktop "$build_dir"/
+            cp "$project_root_dir"/src/main/resources/de/thomas_oster/visicut/gui/resources/icon.png "$build_dir"/visicut.png
+
+            cp -R "$visicut_dir" "$build_dir"/visicut
+
+            # we use the same Linux launcher for every bundle, so let's put it into our newly created visicut dir
+#            cp "$distribute_dir"/linux/VisiCut.Linux "$build_dir"/visicut/
+
+            cp "$distribute_dir"/linux/appimagecraft.yml .
+            appimagecraft
+
+            mv VisiCut*.AppImage "$old_cwd"
+
+            popd
+            ;;
+
+        linux-checkinstall)
+            pushd "$build_dir"
+
+            # copy helper scripts
+            cp "$distribute_dir/linux/*-pak" .
+
+            test -f /usr/bin/visicut && { echo "error: please first uninstall visicut"; exit 1; }
+
+            fakeroot checkinstall --fstrans --reset-uid --type debian --install=no -y \
+                --pkgname visicut --pkgversion "$VERSION" --arch all --pkglicense LGPL \
+                --pkggroup other --pkgsource "http://visicut.org" \
+                --pkgaltsource "https://github.com/t-oster/VisiCut" \
+                --pakdir distribute/ --maintainer "'Thomas Oster <thomas.oster@rwth-aachen.de>'" \
+                --requires 'bash,openjdk-11-jre\|openjdk-17-jre,potrace' \
+                make install -e PREFIX=/usr
+
+            popd
+            ;;
+
+        linux-archlinux)
+            ARCHVERSION=${VERSION//-/_}
+            pushd "$build_dir"
+            cp "$distribute_dir"/linux/* .
+            sed -i "s#pkgver=VERSION#pkgver=$ARCHVERSION#g" PKGBUILD
+            makepkg -p PKGBUILD
+            mv *.pkg.tar.xz "$old_cwd"
+            popd
+            ;;
+
+        *)
+            log "Unknown target $target, skipping"
+            continue
+            ;;
+    esac
+
+    cleanup
+done
