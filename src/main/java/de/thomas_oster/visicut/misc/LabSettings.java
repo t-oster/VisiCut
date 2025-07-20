315106bcffb36f645b507ea921a4448d17a8d468
Kilian Kreibich <kilian@kreibich.xyz>
2025-07-20 19:10:29 +0200

    Add FabLab Ansbach

--- src/main/java/de/thomas_oster/visicut/misc/LabSettings.java
+++ src/main/java/de/thomas_oster/visicut/misc/LabSettings.java
@@ -81,6 +81,7 @@ public class LabSettings
     result.add(new LabSettings("Germany, Paderborn: FabLab Paderborn e.V.", "https://github.com/fablab-paderborn/visicut-settings/archive/master.zip"));
     result.add(new LabSettings("Germany, Reutlingen: INNOPORT / MakeRTreff", "https://github.com/InnoportReutlingen/VisiCut-Settings/archive/main.zip"));
     result.add(new LabSettings("Germany, Veitsbronn: FabLab Landkreis Fürth e.V.", "https://github.com/falafue/visicut-settings/archive/master.zip"));
+    result.add(new LabSettings("Germany, Ansbach: FabLab Ansbach e.V.", "http://github.com/FabLab-Ansbach/visicut-settings/archive/main.zip"));
     result.add(new LabSettings("India, Bangalore: Makerspace BLR", "https://github.com/pallavagarwal07/Makerspace-BLR-Visicut-Settings/archive/master.zip"));
     result.add(new LabSettings("Netherlands, Amersfoort: FabLab", "https://github.com/Fablab-Amersfoort/visicut-settings/archive/master.zip"));
     result.add(new LabSettings("Netherlands, Enschede: TkkrLab", "https://github.com/TkkrLab/visicut-settings/archive/master.zip"));
